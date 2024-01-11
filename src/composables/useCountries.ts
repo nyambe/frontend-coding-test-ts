586ab52e123a892ea686a4c4ba070da846065606
Samuel Ebuka <samuel.ebuka@gmail.com>
2024-01-11 18:57:33 +0100

    new direcction

--- src/composables/useCountries.ts
+++ src/composables/useCountries.ts
@@ -1,7 +1,6 @@
-import { continents } from 'countries-list';
-import { ref, computed } from 'vue';
-import { ApolloClient, createHttpLink, InMemoryCache, gql, ApolloError } from '@apollo/client/core'
+import { ref, computed, onMounted } from 'vue';
 import { provideApolloClient, useQuery } from "@vue/apollo-composable";
+import { ApolloClient, createHttpLink, InMemoryCache, gql, ApolloError } from '@apollo/client/core'
 
 export interface Continent {
 	code: string;
@@ -12,20 +11,32 @@ export interface GetContinentsQueryResult {
 	continents: Continent[];
 }
 
-// HTTP connection to the API
+export interface Language {
+	code: string;
+	name: string;
+}
+
+export interface Country {
+	code: string;
+	name: string;
+	currencies: string[];
+	currency: string;
+	emoji: string;
+	languages: Language[];
+}
+
 const httpLink = createHttpLink({
-	// You should use an absolute URL here
 	uri: 'https://countries.trevorblades.com',
 })
 
-// Cache implementation
 const cache = new InMemoryCache()
 
-// Create the apollo client
 const apolloClient = new ApolloClient({
 	link: httpLink,
 	cache,
 })
+
+provideApolloClient(apolloClient);
 
 const GET_CONTINENTS = gql`
 	query GetContinents {
@@ -36,32 +47,142 @@ const GET_CONTINENTS = gql`
 	}
 `;
 
-const continents = ref<GetContinentsQueryResult>()
+const GET_COUNTRIES_BY_CONTINENT = gql`
+query GetCountries($code: String) {
+	countries(filter: { continent: { eq: $code }, currency: { ne: "EUR" } }) {
+		code
+		name
+		currencies
+		currency
+		emoji
+		languages{
+			code
+			name
+		}
+	}
+}
+`;
+
+const continents = ref<Continent[]>()
 const loadingStatus = ref(false)
 const errorMessage = ref<ApolloError | null>()
 
 
-function getContinents() {
-	const query = provideApolloClient(apolloClient)(() => useQuery<GetContinentsQueryResult>(GET_CONTINENTS))
-	const { result, loading, error } = query
-	const continents = result.value?.continents
-	return {
-		result,
-		continents,
-		loading,
-		error
-	}
+// Random Continent
+
+const currentContinent = ref<Continent>();
+const showContinentName = ref(false);
+const showTimeout = ref(500);
+const currentNumber = ref(0);
+
+function setRandomNumber(length: number) {
+	return Math.floor(Math.random() * length);
+}
+
+function setShowContentinent() {
+	showContinentName.value = true;
+	setTimeout(() => {
+		showContinentName.value = false;
+	}, showTimeout.value);
 }
 
 
-console.log(continents)
+function getCountries(code: string) {
+
+
+
+	const { result, loading, error } = useQuery(GET_COUNTRIES_BY_CONTINENT, {
+		code: code || currentContinent.value?.code,
+	});
+
+	const countries = computed(() => result.value?.countries || []);
+	const currentCountry = computed(() => {
+		if (continents.value) {
+			console.log('continents', countries.value)
+			const randomIndex = Math.floor(Math.random() * countries.value.length);
+			return countries.value[randomIndex];
+		}
+		return undefined;
+	})
+	const currencyMap = computed(() => {
+		const map = new Map();
+
+		countries.value.forEach(country => {
+			country.currencies.forEach(currency => {
+				if (!map.has(currency)) {
+					map.set(currency, []);
+				}
+				map.get(currency).push(country.name);
+			});
+		});
+
+		return map;
+	});
+
+	return { countries, loading, error, currencyMap, currentCountry };
+
+
+}
+
+
+
+function getContinents() {
+	const { result, loading, error } = useQuery<GetContinentsQueryResult>(GET_CONTINENTS)
+
+	const continents = computed(() => result.value?.continents)
+
+	const currentContinent = computed(() => {
+		if (continents.value) {
+			console.log('continents', continents.value)
+			setShowContentinent()
+			const randomIndex = Math.floor(Math.random() * continents.value.length);
+			return continents.value[randomIndex];
+		}
+		return undefined;
+	})
+
+	function checkAnswerContinent(code) {
+		console.log('code', code, currentContinent.value)
+		if (!currentContinent.value || !continents.value || !result.value) {
+			return
+		}
+		if (currentContinent.value?.code === code) {
+
+			alert('Correct!');
+			const res = getCountries(code);
+
+			console.log('res', res?.countries.value)
+
+			// result.value?.continents?.splice(continents.value.indexOf(currentContinent.value), 1);
+		} else {
+			alert('Wrong! Try again.');
+		}
+		// selectRandomContinent(); // Select a new continent for the next round
+	}
+
+	console.log('dontine', currentContinent.value)
+	return {
+		result,
+		loading,
+		error,
+		continents,
+		currentContinent,
+		checkAnswerContinent
+	}
+}
 
 export function useCountries() {
 	return {
 		continents,
 		loadingStatus,
 		errorMessage,
-		getContinents
+		currentContinent,
+		showContinentName,
+		getContinents,
+		setShowContentinent,
+		setRandomNumber,
+		getCountries,
+
 	}
 }
 
--- src/views/Countries.vue
+++ src/views/Countries.vue
@@ -2,29 +2,36 @@
 	<div class="w-full">
 		<div v-if="loading" class="flex items-start justify-center w-full h-screen pt-20">
 			<svg class="w-12 h-12 text-teal-600 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-    </svg>
+				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+				<path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+			</svg>
 		</div>
-		<div v-if="result" class="grid w-full max-w-4xl gap-2 mx-auto md:grid-cols-3">
 
-			<button class="py-5 text-3xl font-black text-center uppercase hover:bg-gray-200" v-for="item in result.continents" v-bind:key="item.code">
-				{{  item.code }}
-			
+		<div class="flex items-center justify-center w-full h-screen">
+			<div v-if="currentContinent && showContinentName" class="w-full p-5 text-6xl font-black text-center">
+					{{ currentContinent.name }}
+		</div>
+
+		<div v-if="continents && !showContinentName" class="grid w-full max-w-4xl gap-2 mx-auto md:grid-cols-3">
+
+
+			<button v-on:click="checkAnswerContinent(item.code)" class="py-5 text-3xl font-black text-center uppercase hover:bg-gray-200" v-for="item in continents" v-bind:key="item.code">
+				{{  item.code }}	
 			</button>
 		
 		</div>
-		<button v-on:click="getContinents">get continents</button>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-// import { countries, continents } from 'countries-list';
 import { useCountries } from '../composables/useCountries';
 
-const { getContinents } = useCountries();
+const { getContinents, showContinentName, getCountries } = useCountries();
 
-const {result, error, loading }	= getContinents()
+const {result, error, loading, continents, currentContinent, checkAnswerContinent }	=  getContinents()
+
+
 
 
 
